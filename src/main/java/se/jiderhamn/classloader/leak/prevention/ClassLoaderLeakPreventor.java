6621af6de4efb097ff2a1ab395fda2721d6e76e4
Mattias Jiderhamn <mattias.jiderhamn@expertsystems.se>
2013-02-21 09:08:07 +0100

    Include fix for leak caused by JSF / Mojarra as of http://java.net/jira/browse/JAVASERVERFACES-2746

--- src/main/java/se/jiderhamn/classloader/leak/prevention/ClassLoaderLeakPreventor.java
+++ src/main/java/se/jiderhamn/classloader/leak/prevention/ClassLoaderLeakPreventor.java
@@ -31,7 +31,8 @@ import java.util.*;
 import java.util.concurrent.ThreadPoolExecutor;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
-import javax.servlet.*;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
 
 /**
  * This class helps prevent classloader leaks.
@@ -346,7 +347,9 @@ public class ClassLoaderLeakPreventor implements javax.servlet.ServletContextLis
     clearBeanELResolverCache();
 
     fixBeanValidationApiLeak();
-
+    
+    fixJsfLeak();
+    
     fixGeoToolsLeak();
     
     // Can we do anything about Google Guice ?
@@ -865,6 +868,54 @@ public class ClassLoaderLeakPreventor implements javax.servlet.ServletContextLis
       }
     }
     
+  }
+  
+  /** 
+   * Workaround for leak caused by Mojarra JSF implementation if included in the container.
+   * See <a href="http://java.net/jira/browse/JAVASERVERFACES-2746">JAVASERVERFACES-2746</a>
+   */
+  protected void fixJsfLeak() {
+    /*
+     Note that since a WeakHashMap is used, it is not the map key that is the problem. However the value is a
+     Map with java.beans.PropertyDescriptor as value, and java.beans.PropertyDescriptor has a Hashtable in which
+     a class is put with "type" as key. This class may have been loaded by the web application.
+
+     One example case is the class org.primefaces.component.menubutton.MenuButton that points to a Map with a key 
+     "model" whose PropertyDescriptor.table has key "type" with the class org.primefaces.model.MenuModel as its value.
+
+     For performance reasons however, we'll only look at the top level key and remove any that has been loaded by the
+     web application.     
+     */
+    
+    Object o = getStaticFieldValue("javax.faces.component.UIComponentBase", "descriptors");
+    if(o instanceof WeakHashMap) {
+      WeakHashMap descriptors = (WeakHashMap) o;
+      final Set<Class> toRemove = new HashSet<Class>();
+      for(Object key : descriptors.keySet()) {
+        if(key instanceof Class && isLoadedByWebApplication((Class)key)) {
+          // For performance reasons, remove all classes loaded in web application
+          toRemove.add((Class) key);
+          
+          // This would be more correct, but presumably slower
+          /*
+          Map<String, PropertyDescriptor> m = (Map<String, PropertyDescriptor>) descriptors.get(key);
+          for(Map.Entry<String,PropertyDescriptor> entry : m.entrySet()) {
+            Object type = entry.getValue().getValue("type"); // Key constant javax.el.ELResolver.TYPE
+            if(type instanceof Class && isLoadedByWebApplication((Class)type)) {
+              toRemove.add((Class) key); 
+            }
+          }
+          */
+        }
+      }
+      
+      if(! toRemove.isEmpty()) {
+        info("Removing " + toRemove.size() + " classes from Mojarra descriptors cache");
+        for(Class clazz : toRemove) {
+          descriptors.remove(clazz);
+        }
+      }
+    }
   }
   
   /** Shutdown GeoTools cleaner thread as of http://jira.codehaus.org/browse/GEOT-2742 */
