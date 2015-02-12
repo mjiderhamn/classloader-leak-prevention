111e331530f5dd3304fd297349603a369e9b1e69
Thomas Scheffler <github@yagee.de>
2015-02-12 12:09:25 +0100

    do not use real imageio plugin, provide a mockup to trigger classloader leak

---
+++ src/test/java/se/jiderhamn/classloader/leak/prevention/ImageIOMockImageInputStreamSPI.java
@@ -0,0 +1,22 @@
+package se.jiderhamn.classloader.leak.prevention;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
+import javax.imageio.spi.ImageInputStreamSpi;
+import javax.imageio.stream.ImageInputStream;
+
+public class ImageIOMockImageInputStreamSPI extends ImageInputStreamSpi {
+
+    @Override
+    public ImageInputStream createInputStreamInstance(Object input, boolean useCache, File cacheDir) throws IOException {
+        throw new IllegalArgumentException("mock class");
+    }
+
+    @Override
+    public String getDescription(Locale locale) {
+        return "mock ImageInputStream provider";
+    }
+
+}
