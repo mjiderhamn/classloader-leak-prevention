2754d3f43edd02c0f3dbdaefc6ddd844869ea167
Mattias Jiderhamn <mattias.jiderhamn@readsoft.com>
2015-10-17 13:04:39 +0200

    Fix ImageIOTest dependency on JEditorPaneTest

--- classloader-leak-prevention/src/test/java/se/jiderhamn/classloader/leak/prevention/ImageIOTest.java
+++ classloader-leak-prevention/src/test/java/se/jiderhamn/classloader/leak/prevention/ImageIOTest.java
@@ -1,10 +1,10 @@
 package se.jiderhamn.classloader.leak.prevention;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import se.jiderhamn.classloader.leak.JUnitClassloaderRunner;
 import se.jiderhamn.classloader.leak.LeakPreventor;
 
@@ -14,9 +14,18 @@ import se.jiderhamn.classloader.leak.LeakPreventor;
 @RunWith(JUnitClassloaderRunner.class)
 @LeakPreventor(ImageIOTest.Prevent.class)
 public class ImageIOTest {
-  
+
   @Test
   public void triggerImageIOLeak() throws Exception {
+    // ImageIO.scanForPlugins() triggers two different leaks, but the preventor only removes one, so we need to avoid
+    // the other one
+    new ClassLoaderLeakPreventor().doInSystemClassLoader(new Runnable() {
+      @Override
+      public void run() {
+        new JEditorPane("text/plain", "dummy");
+      }
+    });
+    
     ImageIO.scanForPlugins();
   }
   
