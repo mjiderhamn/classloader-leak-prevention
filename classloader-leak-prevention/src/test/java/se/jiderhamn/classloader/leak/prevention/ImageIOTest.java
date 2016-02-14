6116049285064c7e2553f6585112c84d584029b1
Mattias Jiderhamn <mattias.jiderhamn@readsoft.com>
2016-02-14 13:47:01 +0100

    Document and make use of @Before for running test setup code outside classloader under test

--- classloader-leak-prevention/src/test/java/se/jiderhamn/classloader/leak/prevention/ImageIOTest.java
+++ classloader-leak-prevention/src/test/java/se/jiderhamn/classloader/leak/prevention/ImageIOTest.java
@@ -3,6 +3,7 @@ package se.jiderhamn.classloader.leak.prevention;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import se.jiderhamn.classloader.leak.JUnitClassloaderRunner;
@@ -14,18 +15,16 @@ import se.jiderhamn.classloader.leak.LeakPreventor;
 @RunWith(JUnitClassloaderRunner.class)
 @LeakPreventor(ImageIOTest.Prevent.class)
 public class ImageIOTest {
+  
+  // ImageIO.scanForPlugins() triggers two different leaks, but the preventor only removes one, so we need to avoid
+  // the other one by running some code in parent classloader
+  @Before
+  public void systemClassLoader() {
+    new JEditorPane("text/plain", "dummy"); // java.awt.Toolkit.getDefaultToolkit()
+  }
 
   @Test
   public void triggerImageIOLeak() throws Exception {
-    // ImageIO.scanForPlugins() triggers two different leaks, but the preventor only removes one, so we need to avoid
-    // the other one
-    new ClassLoaderLeakPreventor().doInSystemClassLoader(new Runnable() {
-      @Override
-      public void run() {
-        new JEditorPane("text/plain", "dummy");
-      }
-    });
-    
     ImageIO.scanForPlugins();
   }
   
