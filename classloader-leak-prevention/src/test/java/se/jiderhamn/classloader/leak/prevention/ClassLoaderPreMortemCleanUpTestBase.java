1af9a632eaa568cf52ffe65f9a4ce57dd1efa2bd
Mattias Jiderhamn <mattias.jiderhamn@readsoft.com>
2016-03-20 11:55:53 +0100

    Update ClassLoaderPreMortemCleanUpTestBase to support generics and by default create ClassLoaderPreMortemCleanUp instance from generic type parameter. #43

--- classloader-leak-prevention/src/test/java/se/jiderhamn/classloader/leak/prevention/ClassLoaderPreMortemCleanUpTestBase.java
+++ classloader-leak-prevention/src/test/java/se/jiderhamn/classloader/leak/prevention/ClassLoaderPreMortemCleanUpTestBase.java
@@ -1,8 +1,11 @@
 package se.jiderhamn.classloader.leak.prevention;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.Collections;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import se.jiderhamn.classloader.leak.JUnitClassloaderRunner;
 import se.jiderhamn.classloader.leak.Leaks;
 
 /**
@@ -10,7 +13,8 @@ import se.jiderhamn.classloader.leak.Leaks;
  * TODO Move this to test framework(?)
  * @author Mattias Jiderhamn
  */
-public abstract class ClassLoaderPreMortemCleanUpTestBase {
+@RunWith(JUnitClassloaderRunner.class)
+public abstract class ClassLoaderPreMortemCleanUpTestBase<C extends ClassLoaderPreMortemCleanUp> {
   
   /** 
    * Test case that verifies that {@link #triggerLeak()} indeed does cause a leak, in case the
@@ -34,11 +38,21 @@ public abstract class ClassLoaderPreMortemCleanUpTestBase {
   protected abstract void triggerLeak() throws Exception;
   
   /** 
-   * Concrete tests should implement this method to return a {@link ClassLoaderPreMortemCleanUp} 
+   * Concrete tests may override this method to return a {@link ClassLoaderPreMortemCleanUp} 
    * that will clean up after the leak triggered by {@link #triggerLeak()}, so that {@link ClassLoader} can be
    * garbage collected.
+   * The default implementation will use the generics parameter type information.
    */
-  protected abstract ClassLoaderPreMortemCleanUp getClassLoaderPreMortemCleanUp();
+  protected C getClassLoaderPreMortemCleanUp() throws IllegalAccessException, InstantiationException {
+    ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
+    Object actualType = genericSuperclass.getActualTypeArguments()[genericSuperclass.getActualTypeArguments().length - 1];
+    
+    Class<C> cleanUpImplClass = (actualType instanceof ParameterizedType) ? 
+        (Class<C>) ((ParameterizedType)actualType).getRawType() :
+        (Class<C>) actualType;
+      
+    return cleanUpImplClass.newInstance();
+  }
 
   /**
    * Concrete tests may override this method, in case they to provide a specific {@link ClassLoaderLeakPreventor}
