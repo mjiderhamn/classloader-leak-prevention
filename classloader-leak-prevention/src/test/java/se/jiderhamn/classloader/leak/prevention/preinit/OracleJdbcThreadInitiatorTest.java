23dc1f8e17773929ef17f733c49c674931e3848c
Mattias Jiderhamn <mattias.jiderhamn@readsoft.com>
2016-04-17 17:51:17 +0200

    Adding @Ignore to OracleJdbcThreadInitiatorTest

--- classloader-leak-prevention/src/test/java/se/jiderhamn/classloader/leak/prevention/preinit/OracleJdbcThreadInitiatorTest.java
+++ classloader-leak-prevention/src/test/java/se/jiderhamn/classloader/leak/prevention/preinit/OracleJdbcThreadInitiatorTest.java
@@ -1,11 +1,13 @@
 package se.jiderhamn.classloader.leak.prevention.preinit;
 
+import org.junit.Ignore;
 import se.jiderhamn.classloader.PackagesLoadedOutsideClassLoader;
 
 /**
  * Test cases for {@link OracleJdbcThreadInitiator}
  * @author Mattias Jiderhamn
  */
+@Ignore // Oracle JDBC driver needs to be available for this test
 @PackagesLoadedOutsideClassLoader(packages = "oracle.", addToDefaults = true)
 public class OracleJdbcThreadInitiatorTest extends PreClassLoaderInitiatorTestBase<OracleJdbcThreadInitiator> {
   
