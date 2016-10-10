f4840840065726b0cfd9c73972a69496906f09ba
Mattias Jiderhamn <mattias.jiderhamn@readsoft.com>
2016-10-10 21:49:55 +0200

    Make Postgresql JDBC timer threads start in leak safe (system) ClassLoader rather than protected ClassLoader. Fixes #60. Avoid Postgresql driver interfering with DriverManagerCleanUpTest.

--- classloader-leak-prevention/classloader-leak-prevention-core/src/test/java/se/jiderhamn/classloader/leak/prevention/cleanup/DriverManagerCleanUpTest.java
+++ classloader-leak-prevention/classloader-leak-prevention-core/src/test/java/se/jiderhamn/classloader/leak/prevention/cleanup/DriverManagerCleanUpTest.java
@@ -1,9 +1,12 @@
 package se.jiderhamn.classloader.leak.prevention.cleanup;
 
+import se.jiderhamn.classloader.PackagesLoadedOutsideClassLoader;
+
 /**
  * Test case for {@link DriverManagerCleanUp}
  * @author Mattias Jiderhamn
  */
+@PackagesLoadedOutsideClassLoader(packages = "org.postgresql", addToDefaults = true) // Postgresql driver not part of test
 public class DriverManagerCleanUpTest extends ClassLoaderPreMortemCleanUpTestBase<DriverManagerCleanUp> {
 
   @Override
--- classloader-leak-prevention/classloader-leak-prevention-core/src/test/java/se/jiderhamn/classloader/leak/prevention/preinit/PostgresqlJdbcThreadInitiatorTest.java
+++ classloader-leak-prevention/classloader-leak-prevention-core/src/test/java/se/jiderhamn/classloader/leak/prevention/preinit/PostgresqlJdbcThreadInitiatorTest.java
@@ -1,5 +1,11 @@
 package se.jiderhamn.classloader.leak.prevention.preinit;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+import org.junit.AfterClass;
 import se.jiderhamn.classloader.PackagesLoadedOutsideClassLoader;
 
 /**
@@ -9,4 +15,14 @@ import se.jiderhamn.classloader.PackagesLoadedOutsideClassLoader;
 @PackagesLoadedOutsideClassLoader(packages = "org.postgresql", addToDefaults = true)
 public class PostgresqlJdbcThreadInitiatorTest extends PreClassLoaderInitiatorTestBase<PostgresqlJdbcThreadInitiator> {
 
+  /** When done, deregister driver to avoid interfering with other tests */
+  @AfterClass
+  public static void tearDown() throws SQLException {
+    final Enumeration<Driver> e = DriverManager.getDrivers();
+    while(e.hasMoreElements()) {
+      final Driver driver = e.nextElement();
+      DriverManager.deregisterDriver(driver);
+    }
+  }
+  
 }
