0b00f7329a6c7651f460e24a76723838a351c821
Mattias Jiderhamn <mattias.jiderhamn@readsoft.com>
2018-06-30 11:14:53 +0200

    Adding cleanup for Reactor Netty resources

---
+++ classloader-leak-prevention/classloader-leak-prevention-core/src/main/java/se/jiderhamn/classloader/leak/prevention/cleanup/ReactorNettyHttpResourcesCleanUp.java
@@ -0,0 +1,29 @@
+package se.jiderhamn.classloader.leak.prevention.cleanup;
+
+import java.lang.reflect.Method;
+
+import se.jiderhamn.classloader.leak.prevention.ClassLoaderLeakPreventor;
+import se.jiderhamn.classloader.leak.prevention.ClassLoaderPreMortemCleanUp;
+
+
+/**
+ * Clean up Reactor Netty resources
+ * @author Mattias Jiderhamn
+ */
+public class ReactorNettyHttpResourcesCleanUp implements ClassLoaderPreMortemCleanUp {
+  @Override
+  public void cleanUp(ClassLoaderLeakPreventor preventor) {
+    final Class<?> clazz = preventor.findClass("reactor.netty.http.HttpResources");
+    if(preventor.isLoadedByClassLoader(clazz)) {
+      final Method shutdown = preventor.findMethod(clazz, "shutdown");
+      if(shutdown != null) {
+        try {
+          shutdown.invoke(null);
+        }
+        catch (Throwable e) {
+          preventor.warn(e);
+        }
+      }
+    }
+  }
+}
