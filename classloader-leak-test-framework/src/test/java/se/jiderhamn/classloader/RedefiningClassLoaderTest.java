809078d2a12d96e1a9bd660db2461309cc713774
Mattias Jiderhamn <mattias.jiderhamn@avamonitoring.com>
2022-01-19 10:36:07 +0100

    Tidy up getPackage() PR

--- classloader-leak-test-framework/src/test/java/se/jiderhamn/classloader/RedefiningClassLoaderTest.java
+++ classloader-leak-test-framework/src/test/java/se/jiderhamn/classloader/RedefiningClassLoaderTest.java
@@ -1,19 +1,21 @@
-package se.jiderhamn.classloader.leak.classloader;
+package se.jiderhamn.classloader;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import se.jiderhamn.classloader.leak.JUnitClassloaderRunner;
 import se.jiderhamn.classloader.leak.Leaks;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 @RunWith(JUnitClassloaderRunner.class)
-public class ClassLoaderTest {
+public class RedefiningClassLoaderTest {
 
     @Test
     @Leaks(false)
     public void getPackage() {
         Package aPackage = com.classloader.test.CustomClass.class.getPackage();
-        Assert.assertNotNull("the package of the class is null", aPackage);
-        Assert.assertEquals("com.classloader.test", aPackage.getName());
+        assertNotNull("Class should have non-null package", aPackage);
+        assertEquals("com.classloader.test", aPackage.getName());
     }
 }
